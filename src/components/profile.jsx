3ce76ed2be8101f975e6971220a3c673f0dcf5ed
Derek Gou <derekgou19@gmail.com>
2024-08-08 16:12:57 -0400

    Team Member Resize

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Profileholder(props){
     return (
         <>
-            <div className="grid w-full h-fit gap-8 min-[900px]:grid-cols-[repeat(auto-fit,minmax(28rem,1fr))] grid-cols-[repeat(auto-fit,minmax(20rem,1fr))]">
+            <div className="grid w-full h-fit gap-8 grid-cols-[repeat(auto-fit,minmax(20rem,1fr))]">
                 {props.children}
             </div>
         </>
@@ -14,7 +14,7 @@ function Profile(props){
     return (
         <>
             <div className="p-8 bg-cilcgrey w-full flex flex-col justify-center items-center">
-                <img className="min-[900px]:w-96 w-64" src={props.image}></img>
+                <img className="w-64" src={props.image}></img>
                 <br/>
                 <h3 className="text-center">{props.name}</h3>
                 <br/>
