549a23b73362fd2e75bbe26b4f3358493a57b35c
derekGou <derekgou19@gmail.com>
2025-07-20 10:19:21 -0400

    more tailwind v4 fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+// tailwind.config.js or tailwind.config.ts (with ts-node)
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -7,18 +7,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "cilcblue": "#57ABC4",
-        "cilcdarkblue": "#358097",
-        "cilcgreen": "#BDD84F",
-        "cilcpurple": "#493C97",
-        "cilcred": "#DB1928",
-        "cilcmedblue": "#0066CC",
-        "cilcgrey": "#F0F0F0",
-        "cilcgray": "#E0E0E0",
-        "deepblue": "#213547",
-        "gold": '#F7D000',
-      }
+        cilcblue: "#57ABC4",
+        cilcdarkblue: "#358097",
+        cilcgreen: "#BDD84F",
+        cilcpurple: "#493C97",
+        cilcred: "#DB1928",
+        cilcmedblue: "#0066CC",
+        cilcgrey: "#F0F0F0",
+        cilcgray: "#E0E0E0",
+        deepblue: "#213547",
+        gold: "#F7D000",
+      },
     },
   },
   plugins: [],
-}
+};
