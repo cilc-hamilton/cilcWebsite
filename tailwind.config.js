1de5794d7b432ea169553ceedf4909439c6213ac
derekGou <derekgou19@gmail.com>
2025-07-20 10:28:08 -0400

    silly new tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,7 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        cilcblue: "#57ABC4",
-        cilcdarkblue: "#358097",
-        cilcgreen: "#BDD84F",
-        cilcpurple: "#493C97",
-        cilcred: "#DB1928",
-        cilcmedblue: "#0066CC",
-        cilcgrey: "#F0F0F0",
-        cilcgray: "#E0E0E0",
-        deepblue: "#213547",
-        gold: "#F7D000",
-      },
+      
     },
   },
   plugins: [],
